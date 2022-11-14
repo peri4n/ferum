b8fb2a0ab7b65bdc14cda88a831d664b016d7692
Fabian Bull <spam+github@fbull.de>
2022-11-14 19:29:23 +0100

    Introduce test module

--- src/bio/alphabet/dna.rs
+++ src/bio/alphabet/dna.rs
@@ -2,14 +2,14 @@ use crate::bio::alphabet::Alphabet;
 use crate::bio::sequence::Seq;
 
 #[derive(PartialEq, Eq, Debug)]
-pub enum Nuc4 { A, C, G, T }
+pub enum Nuc4 {
+    A,
+    C,
+    G,
+    T,
+}
 
-const NUCLEOTIDES4: [Nuc4; 4] = [
-    Nuc4::A,
-    Nuc4::C,
-    Nuc4::G,
-    Nuc4::T
-];
+pub(crate) const NUCLEOTIDES4: [Nuc4; 4] = [Nuc4::A, Nuc4::C, Nuc4::G, Nuc4::T];
 
 #[derive(Debug)]
 pub struct Dna4;
@@ -40,13 +40,31 @@ impl Alphabet for Dna4 {
             'c' | 'C' => Nuc4::C,
             'g' | 'G' => Nuc4::G,
             't' | 'T' => Nuc4::T,
-            _ => Nuc4::A
+            _ => Nuc4::A,
         }
     }
 }
 
-#[test]
-fn standard_iupac_alphabet() {
-    assert_eq!(Dna4.size(), 4);
-    assert_eq!(Dna4.elements(), NUCLEOTIDES4);
+#[cfg(test)]
+mod tests {
+    use crate::bio::alphabet::Alphabet;
+    use crate::bio::alphabet::dna::{NUCLEOTIDES4, Nuc4};
+    use super::Dna4;
+
+
+    #[test]
+    fn standard_iupac_alphabet() {
+        assert_eq!(Dna4.size(), 4);
+        assert_eq!(Dna4.elements(), NUCLEOTIDES4);
+    }
+
+    #[test]
+    fn create_new_seq() {
+        let seq = Dna4::new(vec!['a', 'c', 'g', 't']);
+
+        assert_eq!(seq.at(0), Nuc4::A);
+        assert_eq!(seq.at(1), Nuc4::C);
+        assert_eq!(seq.at(2), Nuc4::G);
+        assert_eq!(seq.at(3), Nuc4::T);
+    }
 }
